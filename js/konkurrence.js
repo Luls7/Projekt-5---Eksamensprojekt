c563006fa31fc8380b4937eac16d5a03688c5fdd
jasm1679 <jdch48625@edu.ucl.dk>
2023-05-26 03:54:37 +0200

    Konkurrence med deltag feedback

---
+++ js/konkurrence.js
@@ -0,0 +1,28 @@
+function konkurrence() {
+    let konkurrence = document.getElementById("blurkonkurrence");
+    if (konkurrence.style.display === "block") {
+      konkurrence.style.display = "none";
+    } else {
+      konkurrence.style.display = "block";
+    }
+  }
+
+
+  function krydskonkurrence() {
+    let krydskonkurrence = document.getElementById("blurkonkurrence");
+    if (krydskonkurrence.style.display === "block") {
+      krydskonkurrence.style.display = "none";
+    } else {
+      krydskonkurrence.style.display = "block";
+    }
+  }
+
+
+  function deltagkonkurrence() {
+    let deltagkonkurrence = document.getElementById("blurkonkurrence");
+    if (deltagkonkurrence.style.display === "block") {
+      deltagkonkurrence.style.display = "none";
+    } else {
+      deltagkonkurrence.style.display = "block";
+    }
+  }
---
+++ js/takfordeltagelse.js
@@ -0,0 +1,18 @@
+function deltagelse() {
+    let deltagelse = document.getElementById("blurdeltagelse");
+    if (deltagelse.style.display === "block") {
+      deltagelse.style.display = "none";
+    } else {
+      deltagelse.style.display = "block";
+    }
+  }
+
+
+  function krydsdeltagelse() {
+    let krydsdeltagelse = document.getElementById("blurdeltagelse");
+    if (krydsdeltagelse.style.display === "block") {
+      krydsdeltagelse.style.display = "none";
+    } else {
+      krydsdeltagelse.style.display = "block";
+    }
+  }
