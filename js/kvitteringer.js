06dc395fe5bc75c1f6fd0e15eb701e9925bafcc5
jasm1679 <jdch48625@edu.ucl.dk>
2023-05-26 03:10:55 +0200

    Kvittering

---
+++ js/kvitteringer.js
@@ -0,0 +1,18 @@
+function kvittering() {
+    let kvittering = document.getElementById("blurkvittering");
+    if (kvittering.style.display === "block") {
+      kvittering.style.display = "none";
+    } else {
+      kvittering.style.display = "block";
+    }
+  }
+
+
+  function krydskvittering() {
+    let krydskvittering = document.getElementById("blurkvittering");
+    if (krydskvittering.style.display === "block") {
+      krydskvittering.style.display = "none";
+    } else {
+      krydskvittering.style.display = "block";
+    }
+  }
