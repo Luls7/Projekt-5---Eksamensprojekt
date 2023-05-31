ee568fe87d002ffd0bff5b6e0ab31a1605e483aa
jasm1679 <jdch48625@edu.ucl.dk>
2023-05-31 10:16:01 +0200

    Update hjerte.js

--- js/hjerte.js
+++ js/hjerte.js
@@ -1,16 +1,16 @@
 let antalhjerter = 19;
 let hjerte = Array(antalhjerter);
 for (i = 0; i<antalhjerter; i++) {
-  hjerte[i] = 0;
+  hjerte[i] = 0; 
 }
 
 function changeHjerte(nr) {
-    let nrid = "hjerte" + nr
-    if (hjerte[nr] === 0) {
-        hjerte[nr] = 1;
-        document.getElementById(nrid).src = "../image/heartbrown.svg";
+    let nrid = "hjerte" + nr 
+    if (hjerte[nr] === 0) { 
+        hjerte[nr] = 1; 
+        document.getElementById(nrid).src = "../image/heartbrown.svg"; 
     } else {
-        hjerte[nr] = 0;
-        document.getElementById(nrid).src = "../image/heartred.svg";
+        hjerte[nr] = 0; 
+        document.getElementById(nrid).src = "../image/heartred.svg"; 
     }
 }
